9126c6e3bb29ccfce39ac8b8e132d8f1e477d5a7
mohammed hinafi <quarkend@live.fr>
2020-12-29 20:55:14 +0100

    premier commit de securite

---
+++ script/index.js
@@ -0,0 +1,116 @@
+const apiUrl = "http://localhost:3000/api/teddies";
+
+//fonction apppel des ours
+async function askTeddies() {
+    let promise = await fetch(apiUrl);
+    let response = await promise.json();
+    return response;
+}
+
+function askAndCreateTeddies() {
+    askTeddies().then(function (response){
+        console.log("test");
+        console.log(response);
+        // response = liste de nounours
+        //boucle pour gérer chaque résultat de la liste
+        response.forEach((product)=> {
+            console.log(product);
+            let result = createHtmlForProduct(product);
+            console.log(result);
+            document.getElementById("produit").appendChild(result);
+            //a partir de chaque élément extrait de la liste 
+            //crée du code html à injecter dans la page 
+
+        })
+    });
+}
+
+
+//fonction de création du code html
+function createHtmlForProduct(product){
+    let container = document.createElement("div");
+    container.classList.add("col-lg-4","col-md-6", "mb-4");
+    container.classList.add("card");
+    let card = document.createElement("div");
+    card.classList.add("card","h-100");
+    let link = document.createElement("a");
+    let img = document.createElement("img");
+    img.classList.add("card-img-top");
+    let newDiv = document.createElement('div')
+
+    newDiv.classList.add('card-body')
+   
+  
+   
+    const h2 = document.createElement('h2')
+    h2.textContent = product.name;
+   
+    const p = document.createElement('p')
+    p.classList.add('moreInfo')
+   
+    p.textContent= product.description;   
+   
+    const button = document.createElement('button')
+    button.classList.add('btn')
+    button.textContent= 'Click to read  more about me '
+   
+    document.body.appendChild(newDiv)
+  
+    newDiv.appendChild(h2)
+    newDiv.appendChild(p)
+    newDiv.appendChild(button)
+   
+   // Adding event handlers
+   
+    function showMore(){
+      document.querySelector('.card-title').style.display ='block'
+   }
+   
+   document.querySelector('.btn').addEventListener('click', showMore)
+
+
+
+
+ 
+ //remplir avec des infos de la bdd
+    img.alt = product.name;
+    img.src = product.imageUrl;
+    link.appendChild(img);
+    card.appendChild(link);
+    container.appendChild(card);
+    card.appendChild(newDiv)
+    return container;
+}
+
+//appel à l'ouverture du site
+askAndCreateTeddies();
+
+/*<div class="card-body">
+<h4 class="card-title">
+  <a href="#">Arnold</a>
+</h4>
+<h5>39.00€</h5>
+<p class="card-text">Lorem ipsum amet.</p>
+</div>*/
+
+
+
+
+
+
+
+
+
+// {/* <div class=""> */}
+// {/* <div class="card h-100"> */}
+//   <a href="#"><img class="card-img-top" src="http://localhost:3000/images/teddy_1.jpg" alt=""></a>  
+//   <div class="card-body">
+//     <h4 class="card-title">
+//       <a href="#">Norbert</a>
+//     </h4>
+//     <h5>29.00€</h5>
+//     <p class="card-text">Lorem ipsum !
+//     </p>
+//   </div>
+// {/* </div> */}
+// {/* </div> */}
