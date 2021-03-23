df139952ea7197cd59ff344e7de1ee8724166c9f
mohammed hinafi <quarkend@live.fr>
2021-03-23 09:23:02 +0100

    Récupération des différents éléments dans le localStorage   ( affichage incomplet  +  remettre le formulaire dans panier.html suite du non reponse

--- script/confirmation.js
+++ script/confirmation.js
@@ -1,25 +1,25 @@
+
 /* Récupération des différents éléments dans le localStorage afin de les afficher au chargement de la page */
-
 const confirmation = document.querySelector(".confirmation-section");
-const objFormulaire = JSON.parse(localStorage.getItem("objFormulaire"));
-const total = JSON.parse(localStorage.getItem("total"));
+const contact = JSON.parse(localStorage.getItem("contact"));
+const orderId = JSON.parse(localStorage.getItem("orderId"));
+const total = JSON.parse(localStorage.getItem('total'));
 
-confirmation.insertAdjacentHTML(
-  "beforeend",
-  `
+confirmation.insertAdjacentHTML("beforeend",`
     <h2>Confirmation de la commande : </h2>
     <ul>
         <li class="confirmation-section__coord">Vos coordonnées : </li>
-          <li>Prénom : ${objFormulaire.prenom}</li>
-
-        <li>Email : ${objFormulaire.email}</li>
-
-      
+        <li>Nom : ${contact.lastName}</li>
+        <li>Prénom : ${contact.firstName}</li>
+        <li>Adresse : ${contact.address}</li>
+        <li>Ville : ${contact.city}</li>
+        <li>Email : ${contact.email}</li>
     </ul>
-    <h3>Total : ${(total / 100).toFixed(2).replace(".", ",")} €</h3>
+    <h3>Total : ${(total/100).toFixed(2).replace(".",",")} €</h3>
+    <h3>Numéro de la commande : </br> ${orderId}</h3>
+    <p>Ce numéro fait office de <strong>preuve d'achat</strong>. Veuillez-le conserver précieusement.</p>
+`);
+localStorage.removeItem('contact');
+localStorage.removeItem('total');
+localStorage.removeItem('orderId');
 
-    
-`
-);
-localStorage.removeItem("objFormulaire");
-localStorage.removeItem("total");
